71e477f8a9a1cb1a387e3390cea88aa6dea131ba
Sandi Andrian <andrian.sandi@gmail.com>
2020-02-19 21:25:14 +0700

    add react helmet

--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Helmet} from 'react-helmet'
 
 class Contact extends React.Component {
   render() {
-    return <h1>Contact</h1>
+    return (
+      <div className="edx-app__contact">
+        <Helmet>
+          <title>Kontak Kami</title>
+        </Helmet>
+        <h1>Contact</h1>
+      </div>
+    )
   }
 }
 export default Contact
