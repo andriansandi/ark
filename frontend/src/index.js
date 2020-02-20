f07993971250d71eef9b6b0ec37ae897390f3ee9
Sandi Andrian <andrian.sandi@gmail.com>
2020-02-20 09:11:28 +0700

    Add react-snap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { hydrate, render } from "react-dom";
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
@@ -17,7 +18,14 @@ const routing = (
     </Router>
   )
 
-ReactDOM.render(routing, document.getElementById('root'));
+// ReactDOM.render(routing, document.getElementById('root'));
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(routing, rootElement);
+} else {
+  render(routing, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
